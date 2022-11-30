2a97cd9b475e7aace497b4c429a442863d9d6db2
Nurbek <nurbekkolbaev81@gmail.com>
2022-11-30 20:10:26 +0600

    задача 10.13.15

---
+++ Homework/задача10.13.15/Program.cs
@@ -0,0 +1,85 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+  /*
+int ShowDec(int num1)
+{
+   int result=0;
+   int num2 =0;
+   if ( num1 < 1000 && num1 >100)
+   {
+       num2 = num1/10;  
+   }
+    result =  num2%10;
+    return result;
+    
+
+    
+   
+}
+
+
+
+Console.WriteLine("Введите трехзначное число : ");
+
+int user_num = Convert.ToInt32(Console.ReadLine());
+int res = ShowDec(user_num);
+
+Console.Write($"The 2nd digit of number {user_num } is {res}");
+
+
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет. (без пеобразования типов)
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6      */
+
+/*
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Findnumber(int num)
+{
+  while (num > 1000)
+       {
+        num = num /10;
+       }
+       return num % 10;
+}
+
+if (number < 0) number = - number;
+if (number > 99)
+{
+  int result = Findnumber(number);
+  Console.WriteLine(result);
+}
+else{
+  Console.WriteLine("Третьей цифры нет ");
+
+}*/
+
+
+
+
+
+
+
+
+/*Задача 15: Напишите программу, которая принимает на вход цифру,
+ обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+ помогите 
+
+
+
+
+
